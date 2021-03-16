2cae1a0643f150f4dce20c32b4785de6353f908e
marcel <marcell.maruli021@gmail.com>
2021-03-16 23:59:49 +0700

    Move Navbar to Navbar's file

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,5 +1,5 @@
-export {default as Header} from '../components/Header.vue';
-export {default as CardAbout } from '../components/CardAbout.vue';
-export {default as CardProduk } from '../components/CardProduk.vue';
-export {default as Navbar } from '../components/Navbar.vue';
+export { default as Header } from '../components/Header.vue';
+export { default as CardAbout } from '../components/CardAbout.vue';
+export { default as CardProduk } from '../components/CardProduk.vue';
+export { default as Navbar } from '../components/Navbar/Navbar.vue';
 
